7dd7441dedf51085583b2f6c6fbcead1e04ab291
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-08-28 10:23:20 +0530

    Update primenumber.dart

--- primenumber.dart
+++ primenumber.dart
@@ -1,1 +1,18 @@
 
+int prime(int a) {
+  var count = 0;
+  for (int i = 2; i <= 100; i++) {
+    if (a % i == 0) count++;
+    if (count > 1) return 0;
+  }
+
+  return a;
+}
+
+main() {
+  print("prime number between 1 to 100");
+
+  for (int i = 0; i <= 100; i++) {
+    if (prime(i) != 0) print("the num  ${prime(i)} is prime");
+  }
+}
