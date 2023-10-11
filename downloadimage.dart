4db265ce68e14619ce1dd16e96f7f51569c36b7e
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-11 10:53:39 +0530

    Update downloadimage.dart

--- downloadimage.dart
+++ downloadimage.dart
@@ -1,1 +1,44 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key }):super(key:key);
+
+  // This widget is the root of your application.
+  @override
+  _HomeState createState()=>_HomeState();
+}
+class _HomeState extends State<Home>{
+  final imageController =TextEditingController();
+  String txtimg = "No value Entered";
+ void _setImg(){
+   setState(() {
+     txtimg = imageController.text;
+   }
+   );
+ } 
+ @override 
+  Widget build(BuildContext context){
+   return Scaffold(
+     appBar: AppBar(
+       title: const Text("Image Example get"),
+       backgroundColor: Colors.black12,
+     ),
+     body: Column(
+       children: [
+         Padding(padding: const EdgeInsets.all(15),
+         child: TextField(
+           decoration: const InputDecoration(labelText: 'Get Image'),
+           controller: imageController,
+         ),
+         )
+       ],
+     ),
+   )
+ }
+}
+
 
