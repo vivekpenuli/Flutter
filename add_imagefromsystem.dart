6976e23af698a1b83dd42af3dfb65bcb2387464e
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-11 10:32:03 +0530

    Update add_imagefromsystem.dart

--- add_imagefromsystem.dart
+++ add_imagefromsystem.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(appBar: AppBar(title: Text('IMAGE DEMO'),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset('image_Src/abc.png',width: 300,height: 300),
+            Image.asset('image_Src/bcd.png',width: 300,height: 300),
+
+
+          ],
+        ),
+      ),
+
+
+
+      ),
+    );
+  }
+}
+
 
