1378d56b0bacc22929bd198f5838fae38082bd81
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-20 09:57:51 +0530

    Update 20third.dart

--- 20third.dart
+++ 20third.dart
@@ -1,1 +1,23 @@
 
+import 'package:flutter/material.dart';
+
+class ThirdScreen extends StatelessWidget {
+  const ThirdScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Third Screen"),),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(onPressed: (){Navigator.pop(context);}, child:Text("Second Screen")),
+            ElevatedButton(onPressed: (){Navigator.pushNamed(context,'/' );}, child: Text("First Screen")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
