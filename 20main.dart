c48dcdb12a02ca929f562eb4dbd2ae1f930f7a35
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-20 09:55:40 +0530

    Update 20main.dart

--- 20main.dart
+++ 20main.dart
@@ -1,1 +1,22 @@
 
+import 'package:flutter/material.dart';
+import 'FirstScreen.dart';
+import 'SecondScreen.dart';
+import 'ThirdScreen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Nmaed Routes Demo',
+      initialRoute: '/',
+      routes: {
+        '/':(context) => const FirstScreen(st:'Dheeraj'),
+        '/second':(context) => const SecondScreen(message:'GoodMorning'),
+        '/third':(context)=>const ThirdScreen(),
+      },
+    )
+  );
+}
+
+
+
