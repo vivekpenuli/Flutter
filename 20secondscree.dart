3be5946c4bbb7b2ab961840363b5dc40620103bb
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-20 09:57:18 +0530

    Update 20secondscree.dart

--- 20secondscree.dart
+++ 20secondscree.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class SecondScreen extends StatelessWidget {
+  final String ?message;
+  const SecondScreen({required this.message});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Second Screen $message"),),
+      body: Center(
+       child: Row(
+         mainAxisAlignment: MainAxisAlignment.center,
+         children:[
+           ElevatedButton(onPressed: (){Navigator.pop(context);}, child:Text("First Screen")),
+           ElevatedButton(onPressed: (){Navigator.pushNamed(context,'/third' );}, child: Text("Third Screen")),
+         ],
+       ),
+      ),
+    );
+  }
+}
+
+
 
