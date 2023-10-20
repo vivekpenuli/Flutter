a03dcb86371c016527ae142b08d30eb3ff2470b3
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-10-20 09:56:17 +0530

    Update 20firstscreen.dart

--- 20firstscreen.dart
+++ 20firstscreen.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+
+class FirstScreen extends StatelessWidget {
+  final String? st;
+  const FirstScreen({required this.st});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen $st'),),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: (){Navigator.pushNamed(context,'/second');},child: Text("Launch Screen"),
+        ),
+      ),
+    );
+  }
+}
 
