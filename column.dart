db23f3b7baead269b2cdddd55a09686f6478921f
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-05 11:16:12 +0530

    Update column.dart

--- column.dart
+++ column.dart
@@ -1,1 +1,30 @@
 
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyColumnApp(),
+    ),
+  );
+}
+
+class MyColumnApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Column Widget Example'),
+      ),
+      body: Column(
+        children: [
+          Text('Item 1'),
+          Text('Item 2'),
+          Text('Item 3'),
+        ],
+      ),
+    );
+  }
+}
+
