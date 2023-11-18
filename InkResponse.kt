e1776d57591d4b572c719319183b214f621f2642
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-18 12:50:03 +0530

    Update InkResponse.kt

--- InkResponse.kt
+++ InkResponse.kt
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('InkResponse Example'),
+        ),
+        body: Center(
+          child: InkResponse(
+            onTap: () {
+              // Handle tap
+              print('Tapped!');
+            },
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Text('Click me'),
+            ),
+            splashColor: Colors.red, // Custom splash color
+            highlightColor: Colors.transparent, // Make the highlight transparent
+          ),
+        ),
+      ),
+    );
+  }
+}
 
