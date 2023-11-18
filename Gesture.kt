c07849e1f40bfd7a9d61c2591009c48a7e70e461
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-18 12:51:39 +0530

    Update Gesture.kt

--- Gesture.kt
+++ Gesture.kt
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('GestureDetector Example'),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              // Handle the tap gesture
+              print('Tapped!');
+            },
+            child: Container(
+              width: 200,
+              height: 200,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  'Tap Me!',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
