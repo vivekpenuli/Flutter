92aa0c92d8f3145bf0904a9a6524099d6083c071
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-18 12:50:45 +0530

    Update InkWell.kt

--- InkWell.kt
+++ InkWell.kt
@@ -1,1 +1,33 @@
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('InkWell Example'),
+        ),
+        body: Center(
+          child: InkWell(
+            onTap: () {
+              // Handle tap
+              print('Tapped!');
+            },
+            child: Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                border: Border.all(),
+              ),
+              child: Text('Click me'),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
