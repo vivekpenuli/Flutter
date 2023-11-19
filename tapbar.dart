d0872929d5a0d281fbaf7da68c2f42e5901c621c
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-19 10:48:51 +0530

    Update tapbar.dart

--- tapbar.dart
+++ tapbar.dart
@@ -1,1 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3, // Specify the number of tabs
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text('TabBarView Example'),
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.home), text: 'Home'),
+                Tab(icon: Icon(Icons.search), text: 'Search'),
+                Tab(icon: Icon(Icons.person), text: 'Profile'),
+              ],
+            ),
+          ),
+          body: TabBarView(
+            children: [
+              // Content of the 'Home' tab
+              Center(child: Text('Home Tab Content')),
+              
+              // Content of the 'Search' tab
+              Center(child: Text('Search Tab Content')),
+              
+              // Content of the 'Profile' tab
+              Center(child: Text('Profile Tab Content')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
 
