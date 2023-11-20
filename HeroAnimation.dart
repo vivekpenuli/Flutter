230fb81bfaa4082aa999538470d401dd6585cffe
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-20 11:47:27 +0530

    Update HeroAnimation.dart

--- HeroAnimation.dart
+++ HeroAnimation.dart
@@ -1,1 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First Screen'),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SecondScreen()),
+          );
+        },
+        child: Hero(
+          tag: 'hero-tag',
+          child: Container(
+            color: Colors.blue,
+            height: 100,
+            width: 100,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Screen'),
+      ),
+      body: Hero(
+        tag: 'hero-tag',
+        child: Container(
+          color: Colors.blue,
+          height: 300,
+          width: 300,
+        ),
+      ),
+    );
+  }
+}
 
