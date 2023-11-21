bdcb9083af877d7c2579d2cb02220cb7aaf54dba
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-21 10:22:21 +0530

    Update Scaffold.dart

--- Scaffold.dart
+++ Scaffold.dart
@@ -1,1 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyScaffoldApp(),
+    ),
+  );
+}
+
+class MyScaffoldApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My Flutter App'),
+      ),
+      body: Center(
+        child: Text('Hello, Flutter!'),  ), ); 
+  } 
+}
+
 
