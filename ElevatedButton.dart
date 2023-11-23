dd87be168ff49f5fb94163ea3df336c7053cfdac
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-23 09:49:21 +0530

    Update ElevatedButton.dart

--- ElevatedButton.dart
+++ ElevatedButton.dart
@@ -1,1 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: MyElevatedButtonApp(),
+    ),
+  );
+}
+
+class MyElevatedButtonApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ElevatedButton Widget Example'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Add your action here
+          },
+          child: Text('Click Me'),
+        ),
+      ),
+    );
+  }
+}
 
