9123c3764868fdf96453ae485abc50aa1e6f3db6
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-23 09:50:14 +0530

    Update container.dart

--- container.dart
+++ container.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Container Example'),
+        ),
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            color: Colors.blue,
+            child: Center(
+              child: Text(
+                'Hello, Container!',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 
