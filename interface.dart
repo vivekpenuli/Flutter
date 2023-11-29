3d8bf8f7f7f2377e3442e0cfef38ea0d52323a1c
vivek penuli <63710710+vivekpenuli@users.noreply.github.com>
2023-11-29 18:06:32 +0530

    Update interface.dart

--- interface.dart
+++ interface.dart
@@ -1,1 +1,22 @@
+class abc{
+  void show(){
+  
+  }
+}
+class bcd implements abc{
+ void show()
+ {
+ print("hello");
+ }
+  void app()
+  {
+    print("ok");
+  }
+}
 
+void main() {
+  bcd obj = new bcd();
+  obj.app();
+ obj.show();
+  print('Hello, World!');
+}
